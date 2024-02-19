4e3d9edd0e4bb5498770e43f4cb081e983fc82b2
Nicolas Alfredo Dell <nicolasalfredodell@hotmail.com.ar>
2024-02-18 21:16:37 -0300

    Cambios

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,19 @@ import { ConfigModule } from '@nestjs/config';
 // Controllers
 import { AuthController } from './auth.controller';
 
+// Entities
+import { Auth } from './entities/auth.entity';
+
 // Services
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [ConfigModule]
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([ Auth ])
+  ]
 })
 export class AuthModule {}
--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,7 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class Auth {
 
+    @PrimaryGeneratedColumn('uuid')
     id: string;
-    lastname: string;
-    name: string;
+
+    @Column('text', {
+        unique: true,
+    })
+    email: string;
 
 }
