e6f75c0dcc1f5f3ea0040158667040d8c8f54fc5
Nicolas Dell <nicolasdell_uta@hotmail.com.ar>
2024-03-19 11:48:11 -0300

    Cambios

--- src/auth/dto/index.ts
+++ src/auth/dto/index.ts
@@ -1,3 +1,3 @@
-export { LoginUserDto } from "./login-user.dto";
-export { RecoveryPasswordDto } from "./recovery-password.dto";
-export { SendMailRecoveryPasswordDto } from "./send-mail-recovery-password.dto";
+export { LoginUserDto } from './login-user.dto';
+export { RecoveryPasswordDto } from './recovery-password.dto';
+export { SendMailRecoveryPasswordDto } from './send-mail-recovery-password.dto';
